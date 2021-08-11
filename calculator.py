34a0cb04d9b6cf03b873066e50b1182b9657041c
ClayTaylor <claydevelops@gmail.com>
2021-08-10 20:58:04 -0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,14 @@
+"""This is a calculator."""
+
+"""Take 2 numbers from a user and add them together to form a new number."""
+
+'''Take x from the user and convert their input into an integer. Store that integer in a variable.'''
+'''Take y from the user and convert their input into an integer. Store that integer in a variable.'''
+'''Take x & y and add them together and store that value in a variable called sum.'''
+'''Print sum back to the user.'''
+
+x = int(input('X: ' ))
+y = int(input('Y: ' ))
+sum = x + y
+
+print(sum)
